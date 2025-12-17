4f7e788251b154f59a2bbaa0ac42a0d83613d432
UmutSarac0 <umutsarac160@gmail.com>
2025-12-17 13:51:04 +0300

    Initialize BoxPuzzle object in main method
    Added initialization of BoxPuzzle object in main method.

--- src/app/BoxPuzzleApp.java
+++ src/app/BoxPuzzleApp.java
@@ -1,7 +1,19 @@
 package app;
 
+import puzzle.BoxPuzzle;
+
+/** 
+ * Main Application Class.
+ * Initializes the puzzle and starts the game.
+ */
 public class BoxPuzzleApp {
     public static void main(String[] args) {
+
+        // The main method should only initalize a BoxPuzzle object.
+        BoxPuzzle game = new BoxPuzzle();
+
+        // Starts the game logic.
+        game.play();
 
     }
 
